1bf20c1ac037db380a6ebd4ed26cde626e8e34a7
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-01-18 13:08:20 -0300

    tests for .each methods

--- test/unit.js
+++ test/unit.js
@@ -198,6 +198,97 @@ describe('apply()', function () {
   });
 });
 
+describe('each()', function () {
+  it('should loop array concurrently', function (done) {
+    var n = 0;
+    function t (i, done) {
+      n++;
+      done();
+    }
+    function d (err, results) {
+      n.should.equal(2);
+      should(err).be.not.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each(['b','c'],t,d);
+  });
+
+  it('should loop object concurrently', function (done) {
+    var n = 0;
+    function t (i, done) {
+      n++;
+      done();
+    }
+    function d (err, results) {
+      n.should.equal(2);
+      should(err).be.not.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each({ a: 'b', b: 'c' }, t, d);
+  });
+
+  it('should short-circuit on error', function (done) {
+    function t (i, done) {
+      done(i);
+    }
+    function d (err, results) {
+      should(err).be.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each(['b','c','e'],t,d);
+  });
+});
+
+describe('each.series()', function () {
+  it('should loop array in a series', function (done) {
+    var n = 0;
+    function t (i, done) {
+      n++;
+      done();
+    }
+    function d (err, results) {
+      n.should.equal(2);
+      should(err).be.not.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each.series(['b','c'],t,d);
+  });
+
+  it('should loop object in a series', function (done) {
+    var n = 0;
+    function t (i, done) {
+      n++;
+      done();
+    }
+    function d (err, results) {
+      n.should.equal(2);
+      should(err).be.not.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each.series({ a: 'b', b: 'c' }, t, d);
+  });
+
+  it('should short-circuit on error', function (done) {
+    var n = 0;
+    function t (i, done) {
+      n++;
+      done(i);
+    }
+    function d (err, results) {
+      n.should.equal(1);
+      should(err).be.ok;
+      should(results).be.not.ok;
+      done();
+    }
+    λ.each.series(['b','c'],t,d);
+  });
+});
+
 describe('map()', function () {
   it('should map array concurrently', function (done) {
     var n = 4;
@@ -210,7 +301,7 @@ describe('map()', function () {
       results.should.eql([4, 5]);
       done();
     }
-    λ.map.series(['b','c'],t,d);
+    λ.map(['b','c'],t,d);
   });
 
   it('should map object concurrently', function (done) {
@@ -224,7 +315,7 @@ describe('map()', function () {
       results.should.eql({ a: 4, b: 5 });
       done();
     }
-    λ.map.series({ a: 'b', b: 'c' }, t, d);
+    λ.map({ a: 'b', b: 'c' }, t, d);
   });
 
   it('should short-circuit on error', function (done) {
@@ -270,112 +361,115 @@ describe('map.series()', function () {
   });
 
   it('should short-circuit on error', function (done) {
+    var n = 0;
     function t (i, done) {
+      n++;
       done(i);
     }
     function d (err, results) {
+      n.should.equal(1);
       should(err).be.ok;
       should(results).be.not.ok;
       done();
     }
     λ.map.series(['b','c'],t,d);
   });
+});
 
-  describe('queue()', function () {
-    it('should queue things', function (done) {
-      var ww;
-      function w (job, done) {
-        ww = true;
-        job.should.equal('a');
-        done();
-      }
-      function d (err) {
-        should(err).be.not.ok;
-        should(ww).be.ok;
-        done();
-      }
-      var q = λ.queue(w);
-      q.push('a', d);
-    });
-
-    it('should pause and resume the queue', function (done) {
-      var ww;
-      function w (job, cb) {
-        ww = true;
-        job.should.equal('a');
-        cb();
-      }
-      function d (err) {
-        should(err).be.not.ok;
-        should(ww).be.ok;
-        done();
-      }
-      var q = λ.queue(w);
-      q.pause();
-      q.push('a', d);
-      q.length.should.equal(1);
-      q.resume();
-    });
-
-    it('should report errors', function (done) {
-      var ww;
-      function w (job, done) {
-        ww = true;
-        job.should.equal('a');
-        done('e');
-      }
-      function d (err) {
-        err.should.equal('e');
-        should(ww).be.ok;
-        done();
-      }
-      var q = λ.queue(w);
-      q.push('a', d);
-    });
+describe('queue()', function () {
+  it('should queue things', function (done) {
+    var ww;
+    function w (job, done) {
+      ww = true;
+      job.should.equal('a');
+      done();
+    }
+    function d (err) {
+      should(err).be.not.ok;
+      should(ww).be.ok;
+      done();
+    }
+    var q = λ.queue(w);
+    q.push('a', d);
   });
 
-  describe('emitter()', function () {
-    it('should just work', function (done) {
-      var thing = { foo: 'bar' };
+  it('should pause and resume the queue', function (done) {
+    var ww;
+    function w (job, cb) {
+      ww = true;
+      job.should.equal('a');
+      cb();
+    }
+    function d (err) {
+      should(err).be.not.ok;
+      should(ww).be.ok;
+      done();
+    }
+    var q = λ.queue(w);
+    q.pause();
+    q.push('a', d);
+    q.length.should.equal(1);
+    q.resume();
+  });
 
-      λ.emitter(thing);
+  it('should report errors', function (done) {
+    var ww;
+    function w (job, done) {
+      ww = true;
+      job.should.equal('a');
+      done('e');
+    }
+    function d (err) {
+      err.should.equal('e');
+      should(ww).be.ok;
+      done();
+    }
+    var q = λ.queue(w);
+    q.push('a', d);
+  });
+});
 
-      should(thing.on).be.ok;
-      should(thing.emit).be.ok;
+describe('emitter()', function () {
+  it('should just work', function (done) {
+    var thing = { foo: 'bar' };
 
-      thing.on('something', function (a, b) {
-        a.should.equal('a');
-        b.should.equal(2);
-        done();
-      });
+    λ.emitter(thing);
 
-      thing.emit('something', 'a', 2);
-    });
+    should(thing.on).be.ok;
+    should(thing.emit).be.ok;
 
-    it('should blow up on error if no listeners', function (done) {
-      var thing = { foo: 'bar' };
-
-      λ.emitter(thing);
-
-      should(thing.on).be.ok;
-      should(thing.emit).be.ok;
-
-      thing.emit.bind(thing, 'error').should.throw();
+    thing.on('something', function (a, b) {
+      a.should.equal('a');
+      b.should.equal(2);
       done();
     });
 
-    it('should work just fine with at least one error listener', function (done) {
-      var thing = { foo: 'bar' };
+    thing.emit('something', 'a', 2);
+  });
 
-      λ.emitter(thing);
+  it('should blow up on error if no listeners', function (done) {
+    var thing = { foo: 'bar' };
 
-      should(thing.on).be.ok;
-      should(thing.emit).be.ok;
+    λ.emitter(thing);
 
-      thing.on('error', function () {
-        done();
-      });
-      thing.emit.bind(thing, 'error').should.not.throw();
+    should(thing.on).be.ok;
+    should(thing.emit).be.ok;
+
+    thing.emit.bind(thing, 'error').should.throw();
+    done();
+  });
+
+  it('should work just fine with at least one error listener', function (done) {
+    var thing = { foo: 'bar' };
+
+    λ.emitter(thing);
+
+    should(thing.on).be.ok;
+    should(thing.emit).be.ok;
+
+    thing.on('error', function () {
+      done();
     });
+    thing.emit.bind(thing, 'error').should.not.throw();
   });
 });
