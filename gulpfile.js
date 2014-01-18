e3d5f08e52b5468183f5941320938cce94409610
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-01-18 13:16:47 -0300

    publish to npm through gulp

--- gulpfile.js
+++ gulpfile.js
@@ -41,14 +41,19 @@ gulp.task('bump', ['build'], function () {
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('release', ['bump'], function () {
+gulp.task('tag', ['bump'], function () {
   var pkg = require('./package.json');
   var v = 'v' + pkg.version;
   var message = 'Release ' + v;
 
-  gulp.src('./')
+  return gulp.src('./')
     .pipe(git.commit(message))
     .pipe(git.tag(v, message))
     .pipe(git.push('origin', 'master'))
     .pipe(gulp.dest('./'));
+});
+
+gulp.task('npm', ['tag'], function (done) {
+  require('child_process').spawn('npm', ['publish'], { stdio: 'inherit' })
+    .on('close', done);
 });
