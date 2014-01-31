cdbb00c707888526e6ebb571be7300512547f6a2
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-01-31 19:03:46 -0300

    indexOf polyfill

--- src/contra.shim.js
+++ src/contra.shim.js
@@ -12,6 +12,27 @@
       }
     };
   }
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (what, start) {
+      if (this === undefined || this === null) {
+        throw new TypeError( '"this" is null or not defined' );
+      }
+      var length = this.length;
+      start = +start || 0;
+      if (Math.abs(start) === Infinity) {
+        start = 0;
+      } else if (start < 0) {
+        start += length;
+        if (start < 0) { start = 0; }
+      }
+      for (; start < length; start++) {
+        if (this[start] === what) {
+          return start;
+        }
+      }
+      return -1;
+    };
+  }
   if (!Array.prototype.filter) {
     Array.prototype.filter = function (fn, ctx) {
       var f = [];
