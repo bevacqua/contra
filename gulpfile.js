39b2e47f3371d4254514661b58593137d4e1de4a
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-02-21 10:39:57 -0300

    added banners

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var pkg = require('./package.json');
 var gulp = require('gulp');
 var bump = require('gulp-bump');
 var git = require('gulp-git');
@@ -7,8 +8,22 @@ var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
 var clean = require('gulp-clean');
 var rename = require('gulp-rename');
+var header = require('gulp-header');
 var uglify = require('gulp-uglify');
 var size = require('gulp-size');
+
+
+var extended = [
+  '/**',
+  ' * <%= pkg.name %> - <%= pkg.description %>',
+  ' * @version v<%= pkg.version %>',
+  ' * @link <%= pkg.homepage %>',
+  ' * @license <%= pkg.license %>',
+  ' */',
+  ''
+].join('\n');
+
+var succint = '// <%= pkg.name %>@v<%= pkg.version %>, <%= pkg.license %> licensed. <%= pkg.homepage %>\n';
 
 gulp.task('lint', function () {
   return gulp.src('./src/*.js')
@@ -28,18 +43,22 @@ gulp.task('clean', function () {
 
 gulp.task('build-shim', ['test', 'clean'], function () {
   return gulp.src('./src/contra.shim.js')
+    .pipe(header(extended, { pkg : pkg } ))
     .pipe(gulp.dest('./dist'))
     .pipe(rename('contra.shim.min.js'))
     .pipe(uglify())
+    .pipe(header(succint, { pkg : pkg } ))
     .pipe(size())
     .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build', ['build-shim'], function () {
   return gulp.src('./src/contra.js')
+    .pipe(header(extended, { pkg : pkg } ))
     .pipe(gulp.dest('./dist'))
     .pipe(rename('contra.min.js'))
     .pipe(uglify())
+    .pipe(header(succint, { pkg : pkg } ))
     .pipe(size())
     .pipe(gulp.dest('./dist'));
 });
