f4e714c102e24ccf40e09f58e0fd984726985ac2
Tarcisio Gruppi <txgruppi@gmail.com>
2014-04-02 13:05:59 -0300

    Fixed miss typed 'iterate'

--- src/contra.js
+++ src/contra.js
@@ -98,7 +98,7 @@
       var tasks = a(collection) ? [] : {};
       keys.forEach(function insert (key) {
         tasks[key] = function iterate (cb) {
-          if (typeof iterate.length === 'number' && iterator.length === 3) {
+          if (typeof iterator.length === 'number' && iterator.length === 3) {
             iterator(collection[key], key, cb);
           } else {
             iterator(collection[key], cb);
